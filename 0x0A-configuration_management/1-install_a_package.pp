83495d53c851bc05498417435d04c97e831ba17e
benop7 <benjaminpetegnong@gmail.com>
2022-09-21 12:08:42 +0100

    task 1 - correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,17 +4,12 @@ package {'python3':
 ensure => 'installed'
 }
 
-exec {'sudo /bin/apt update':
-before => Package['python3']
-}
-
-package {'pip3':
-ensure  => 'installed',
-require => Exec['sudo /bin/apt update']
+package {'python3-pip':
+ensure  => 'installed'
 }
 
 package {'flask':
 ensure   => '2.1.0'
 provider => 'pip3'
-require  => Package['pip3']
+require  => Package['python3-pip']
 }
