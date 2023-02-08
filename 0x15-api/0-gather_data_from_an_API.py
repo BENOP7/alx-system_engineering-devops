6665be40baeb828053a7add85b77664e6e332a7c
benjamin <you@example.com>
2023-02-08 23:28:11 +0000

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+    Take employeeId and get todo list using an API
+'''
+
+import urllib.request as rq
+import json
+import sys
+
+idx = int(sys.argv[1])
+
+
+resp = rq.urlopen("https://jsonplaceholder.typicode.com/todos/")
+resp = resp.read()
+dic = json.loads(resp)
+resp = rq.urlopen("https://jsonplaceholder.typicode.com/users/" + str(idx))
+resp = resp.read()
+user = json.loads(resp)
+titles = []
+count = 0
+
+for task in dic:
+    if task.get("userId") == idx:
+        count += 1
+        if task.get("completed"):
+            titles.append(task.get("title"))
+
+print("Employee " + user.get('name') + "is done with tasks(" + str(len(titles))
+      + "/" + str(count) + "):")
+for title in titles:
+    print("\t", title)
