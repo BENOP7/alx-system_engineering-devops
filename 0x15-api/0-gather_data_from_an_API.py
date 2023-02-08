b24617c6639fba7b9a67f8f8f948c0a6eb727617
benjamin <you@example.com>
2023-02-08 23:41:12 +0000

    documented file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,29 +5,30 @@
 '''
 
 
-import urllib.request as rq
-import json
-import sys
+if __name__ == '__main__':
+    import json
+    import sys
+    import urllib.request as rq
 
-idx = int(sys.argv[1])
+    idx = int(sys.argv[1])
 
 
-resp = rq.urlopen("https://jsonplaceholder.typicode.com/todos/")
-resp = resp.read()
-dic = json.loads(resp)
-resp = rq.urlopen("https://jsonplaceholder.typicode.com/users/" + str(idx))
-resp = resp.read()
-user = json.loads(resp)
-titles = []
-count = 0
+    resp = rq.urlopen("https://jsonplaceholder.typicode.com/todos/")
+    resp = resp.read()
+    dic = json.loads(resp)
+    resp = rq.urlopen("https://jsonplaceholder.typicode.com/users/" + str(idx))
+    resp = resp.read()
+    user = json.loads(resp)
+    titles = []
+    count = 0
 
-for task in dic:
-    if task.get("userId") == idx:
-        count += 1
-        if task.get("completed"):
-            titles.append(task.get("title"))
+    for task in dic:
+        if task.get("userId") == idx:
+            count += 1
+            if task.get("completed"):
+                titles.append(task.get("title"))
 
-print("Employee " + user.get('name') + "is done with tasks(" + str(len(titles))
-      + "/" + str(count) + "):")
-for title in titles:
-    print("\t", title)
+    print("Employee " + user.get('name') + "is done with tasks(" +
+          str(len(titles)) + "/" + str(count) + "):")
+    for title in titles:
+        print("\t", title)
